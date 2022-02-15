a6530dadf2ff5263b9ff194aa6c9edb75534abd3
winter_is_here <maeid.kh@gmail.com>
2022-02-15 17:27:04 +0100

    - Intro updated

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,23 +1,21 @@
 import React from "react";
 
-const Button = (props: any) => {
-	const { primary } = props;
+type ButtonProps = {
+	children: React.ReactNode;
+	primary?: Boolean;
+};
 
-	if (primary == true) {
-		<button
-			className="px-12 py-[14px] text-center text-[21px] font-bold rounded-full border border-primary-600 text-white bg-gradient-to-r from-primary-500 via-primary-500 to-purple-500"
-			{...props}
-		>
-			{props.children}
-		</button>;
-	}
-
+const Button = ({ children, primary = false }: ButtonProps) => {
 	return (
 		<button
-			className="px-12 py-[14px] text-center text-[21px] font-bold rounded-full text-primary-500 bg-primary-700"
-			{...props}
+			className={`px-12 py-[14px] text-center text-[21px] font-bold rounded-full border transition hover:-translate-y-1 ${
+				primary
+					? "border-primary-700 text-white bg-gradient-to-r from-primary-500 via-primary-500 to-purple-500"
+					: "border-primary-500 text-primary-500 bg-primary-700"
+			}`}
+			// {...props}
 		>
-			{props.children}
+			{children}
 		</button>
 	);
 };
--- components/Intro.tsx
+++ components/Intro.tsx
@@ -1,11 +1,17 @@
 import Link from "next/link";
-import React from "react";
 import Logo from "components/Logo";
+import React from "react";
+import Image from "next/image";
+
 import Button from "components/Button";
+import LogoShadow from "assets/img/logo-shadow.png";
 
 const Intro = () => {
 	return (
 		<header className="px-6 py-8 lg:py-[88px] bg-primary-700 text-faded">
+			<div className="w-[540px] absolute -left-16 -top-14">
+				<img src={LogoShadow.src.toString()} alt="Logo preview" />
+			</div>
 			<div className="h-[104px] max-w-[1780px] mx-auto flex justify-between items-center">
 				<div className="w-28">
 					<Logo />
@@ -43,17 +49,20 @@ const Intro = () => {
 				</nav>
 			</div>
 
-			<div className="max-w-[1086px]">
-				<h1 className="">
-					Manage all your leads <span className="">Like a pro</span>
+			<div className="max-w-[1086px] mx-auto text-center mt-40">
+				<h1 className="text-6xl font-bold uppercase leading-snug mb-9">
+					Manage all your leads{" "}
+					<span className="block text-8xl text-primary-500">
+						Like a pro
+					</span>
 				</h1>
-				<p>
+				<p className="text-3xl leading-snug">
 					ConduitCRM exists to streamline and simplify lead-processing
 					pipelines, so that real estate and loan professionals can
 					focus more on helping clients and less on managing the
 					process.
 				</p>
-				<div className="flex gap-5">
+				<div className="flex justify-center gap-5 mt-20">
 					<Link href="/">
 						<a className="block">
 							<Button>Explore more</Button>
