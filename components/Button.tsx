df5f36fb9e76bc234bac02aaa081b58a0d7aa859
Antonio Bologna <antonio.bologna@gmail.com>
2022-02-18 10:17:20 -0600

    fix: font size, button component

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,23 +1,23 @@
 import React from "react";
 
 type ButtonProps = {
-	children: React.ReactNode;
-	primary?: Boolean;
+  children: React.ReactNode;
+  primary?: Boolean;
 };
 
 const Button = ({ children, primary = false }: ButtonProps) => {
-	return (
-		<button
-			className={`px-12 py-[14px] text-center text-[21px] font-bold rounded-full border transition hover:-translate-y-1 ${
-				primary
-					? "border-primary-700 text-white bg-gradient-to-r from-primary-500 via-primary-500 to-purple-500"
-					: "border-primary-500 text-primary-500 bg-primary-700"
-			}`}
-			// {...props}
-		>
-			{children}
-		</button>
-	);
+  return (
+    <button
+      className={`border px-12 py-[14px] text-center text-[18px] font-bold rounded-full transition hover:-translate-y-1 ${
+        primary
+          ? "border-0 text-white bg-gradient-to-r from-primary-500 via-primary-500 to-purple-500"
+          : "text-primary-500 border-blue-500"
+      }`}
+      // {...props}
+    >
+      {children}
+    </button>
+  );
 };
 
 export default Button;
