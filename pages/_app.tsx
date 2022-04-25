60e74cd6598011a9fdd453fd29862d53dc5e642a
Antonio Bologna <100706985+antoniobologna-series@users.noreply.github.com>
2022-04-24 21:15:50 -0500

    EO optimizations

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,6 +8,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
+        <title>Conduit | The real estate platform </title>
         <link
           rel="apple-touch-icon"
           sizes="180x180"
@@ -29,6 +30,20 @@ function MyApp({ Component, pageProps }: AppProps) {
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
         <meta name="msapplication-TileColor" content="#603cba" />
         <meta name="theme-color" content="#ffffff"></meta>
+        <meta name="title" content="Conduit | The real estate platform" />
+        <meta
+          name="description"
+          content="Streamlining your business with solutions for real estate professionals to focus more on helping clients and less on managing the process."
+        />
+        <meta
+          name="keywords"
+          content="real estate, crm, real estate agents, real estate brokers, loan professionals, loan officers, conduit, conduit crm, conduitcrm, lead processing, customer relationship management, real estate saas, real estate platform"
+        />
+        <meta name="robots" content="index, follow" />
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="language" content="English" />
+        <meta name="revisit-after" content="30 days" />
+        <meta name="author" content="ConduitCRM LLC"></meta>
       </Head>
       <Layout>
         <Component {...pageProps} />
